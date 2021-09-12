4894f336a035ef7a36f677598a4cd12671ad1bf0
hemhem <takutookamoto0427@gmail.com>
2021-09-12 17:37:30 -0400

    Prepared Layout

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,11 @@
+import { Container } from '@material-ui/core';
+
+function Landing() {
+  return (
+    <Container maxWidth="sm">
+      <div>Hello World</div>
+    </Container>
+  );
+}
+
+export default Landing;
